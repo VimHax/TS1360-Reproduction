e19c52b10ad496484c32878b04f6e43073d2fbc3
VimHax <vimukthi.weerabahu@gmail.com>
2025-10-21 21:49:06 +0000

    Initial commit

---
+++ packages/app/src/index.ts
@@ -0,0 +1,4 @@
+import { test } from '@app/project-state';
+import { Selection } from 'prosemirror-state';
+
+test() satisfies Selection;
---
+++ packages/project-state/src/index.ts
@@ -0,0 +1,5 @@
+import { Selection } from 'prosemirror-state';
+
+export function test(): Selection {
+	return 0 as unknown as Selection;
+}
