48799c731cda874b4de3e348ae563902f8885e2e
VimHax <vimukthi.weerabahu@gmail.com>
2025-10-23 15:18:21 +0000

    Minimize further

---
+++ packages/project-state/lib/index.test.ts
@@ -0,0 +1,4 @@
+import type { Selection } from 'prosemirror-state';
+import { getSelection } from '.';
+
+const a: Selection = getSelection();
---
+++ packages/project-state/lib/index.ts
@@ -0,0 +1,5 @@
+import type { Selection } from "prosemirror-state";
+
+export function getSelection(): Selection {
+    return {} as unknown as Selection;
+}
